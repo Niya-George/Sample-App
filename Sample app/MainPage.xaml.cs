6f3206c1a2ffa2bb72eff8b6b052dd1b5bcf44b4
Niya-George <niya.nediyathara@people10.com>
2024-07-05 17:48:03 +0530

    added sample app

---
+++ Sample app/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Sample_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            string validUsername = "Niya_George";
+            string validPassword = "Pass@123";
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                if (username == validUsername && password == validPassword)
+                {
+                    
+                    Navigation.PushAsync(new Page2());
+                }
+                else
+                {
+                    
+                    DisplayAlert("Error","Invalid username or password.","OK");
+                } }
+            else
+            {
+                
+                DisplayAlert("Error","Username and password are required.","OK");
+            }
+        }
+    }
+}
---
+++ Sample app/Page2.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sample_app
+{
+
+    public partial class Page2 : ContentPage
+    {
+
+
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
